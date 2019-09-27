f6e77042949ca4ff3058d5dc8a2ebbb15415d1c5
Henrik Andersson <hegge.nu@gmail.com>
2019-09-27 22:29:55 +0200

    added custom lambda trigger for cb

---
+++ bootstrap/lambda.py
@@ -0,0 +1,66 @@
+import logging
+import boto3
+import cfnresponse
+
+
+codebuild_client = boto3.client('codebuild')
+
+DEFAULT_LOGGING_LEVEL = logging.INFO
+logging.basicConfig(format='[%(levelname)s] %(message)s', level=DEFAULT_LOGGING_LEVEL)
+logger = logging.getLogger(__name__)
+logger.setLevel(DEFAULT_LOGGING_LEVEL)
+
+def start_build(resource_properties):
+    project_name = resource_properties.get('ProjectName')
+    if (not project_name):
+        raise ValueError(
+          'missing ProjectName resource property'
+        )
+
+    response = codebuild_client.start_build(
+        projectName=project_name
+    )
+    return response
+
+def handler(event, context):
+    logger.info('event: {}'.format(cfnresponse.json_dump_format(event)))
+    request_type = event.get('RequestType')
+    resource_properties = event.get('ResourceProperties')
+
+    response = {}
+    response_status = cfnresponse.SUCCESS
+    request_id = ''
+    reason = ''
+
+    if (request_type in ['Create', 'Update']):
+        try:
+            start_build_response = start_build(resource_properties)
+            logger.info(
+                'start_build response: {}'.format(
+                    cfnresponse.json_dump_format(start_build_response)
+                )
+            )
+            # only return specific fields to prevent "response object is too long" errors
+            response = {
+              'build_id':  start_build_response['build']['id'],
+              'project_name':  start_build_response['build']['projectName'],
+              'arn':  start_build_response['build']['arn'],
+            }
+            response_status = cfnresponse.SUCCESS
+            request_id = start_build_response['ResponseMetadata']['RequestId']
+            reason = 'Create'
+        except Exception as e:
+            error = 'failed to start build: {}'.format(e)
+            logger.error(error)
+            response_status = cfnresponse.FAILED
+            reason = error
+            pass
+
+    cfnresponse.send(
+        event,
+        context,
+        response_status,
+        response,
+        request_id,
+        reason
+    )
